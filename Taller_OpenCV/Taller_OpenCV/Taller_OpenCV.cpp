41e5e4abc489aeb117f0c88f116d8e124fc27e4d
PePe-14 <73206031+PePe-14@users.noreply.github.com>
2021-10-10 19:57:13 -0300

    Primera_Entrega

---
+++ Taller_OpenCV/Taller_OpenCV/Taller_OpenCV.cpp
@@ -0,0 +1,48 @@
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/objdetect.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+//Image
+int main()
+{
+    string path = "Resources/imagen2.jpg";
+    Mat img = imread(path);
+
+    CascadeClassifier faceCascade;
+    faceCascade.load("haarcascade_frontalface_default.xml");
+
+    if (faceCascade.empty()) { cout << "no se cargo el archivo" << endl; }
+
+    vector<Rect> faces;
+    faceCascade.detectMultiScale(img,faces,1.1,10);
+
+    for (int i = 0; i < faces.size(); i++)
+    {
+        rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 0,255),3);
+    }
+    
+    imshow("Image", img);
+    waitKey(0);
+}
+
+
+/*/ Webcam
+/int main()
+{
+  
+    VideoCapture cap(0);
+    Mat img;
+
+    while (true) {
+        
+        cap.read(img);
+
+        imshow("Image", img);
+        waitKey(1);
+    }
+}*/
